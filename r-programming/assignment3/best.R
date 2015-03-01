5c06a03e2a7e6fe736143dc34567d74f3027ae77
Brian Dunnette <brian@dunnette.us>
2015-03-01 15:04:59 -0600

    working version of best() function - should clean up using one function and passing desired outcome

--- r-programming/assignment3/best.R
+++ r-programming/assignment3/best.R
@@ -7,10 +7,36 @@ best <- function(state=FALSE, outcome=FALSE) {
     if(state == FALSE || !state %in% outcomes$State) stop ("invalid state")
     if(outcome == FALSE || !outcome %in% outcomeChoices) stop ("invalid outcome")
     
-    ## Return hospital name in that state with lowest 30-day death
+    ## Return hospital name in that state with lowest 30-day death rate
+    
+    # Get all hospitals in the specified state
+    stateHospitals <- outcomes[outcomes$State == state,]
+    
+    # Would be ideal to pass outcome to function rather than repeating if/else
+    if (outcome == "heart attack") {
+        # Coerce mortality to numeric value, so we can eliminate NAs
+        stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack <- as.numeric(as.character(stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack))
+        # Remove hospitals without mortality value
+        stateHospitals <- stateHospitals[complete.cases(stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack),]
+        # Sort hospitals by given outcome
+        stateHospitals <- stateHospitals[order(stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack),]
+    } else if (outcome == "heart failure") {
+        # Coerce mortality to numeric value, so we can eliminate NAs
+        stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure <- as.numeric(as.character(stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure))
+        # Remove hospitals without mortality value
+        stateHospitals <- stateHospitals[complete.cases(stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),]
+        # Sort hospitals by given outcome
+        stateHospitals <- stateHospitals[order(stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),]
+    } else if (outcome == "pneumonia") {
+        # Coerce mortality to numeric value, so we can eliminate NAs
+        stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia <- as.numeric(as.character(stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))
+        # Remove hospitals without mortality value
+        stateHospitals <- stateHospitals[complete.cases(stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia),]
+        # Sort hospitals by given outcome
+        stateHospitals <- stateHospitals[order(stateHospitals$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia),]
+    }  
+    # Print name of hospital with lowest value for given outcome
+    print(stateHospitals[1,2])
 
-
-    ## rate
-
-
+    
 }
