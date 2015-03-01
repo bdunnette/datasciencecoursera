dee3f117b35b7c40ddc5bf4a5f7cf38243d09fb9
Brian Dunnette <brian@dunnette.us>
2015-02-28 18:30:13 -0600

    adding assignment2

--- r-programming/assignment1/corr.R
+++ r-programming/assignment1/corr.R
@@ -1,25 +1,25 @@
 corr <- function(directory, threshold = 0) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
-  
-  ## 'threshold' is a numeric vector of length 1 indicating the
-  ## number of completely observed observations (on all
-  ## variables) required to compute the correlation between
-  ## nitrate and sulfate; the default is 0
-  
-  corr <- numeric()
-  
-  file.list <- list.files(path=directory, full.names=TRUE)
-  
-  for(filename in file.list){
-    obs <- read.csv(filename, header=TRUE)
-    good <- complete.cases(obs)
-    if(sum(good) > threshold){
-      complete <- obs[good,]
-      corr <- c(corr, cor(complete[,"sulfate"], complete[,"nitrate"]))
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'threshold' is a numeric vector of length 1 indicating the
+    ## number of completely observed observations (on all
+    ## variables) required to compute the correlation between
+    ## nitrate and sulfate; the default is 0
+    
+    corr <- numeric()
+    
+    file.list <- list.files(path=directory, full.names=TRUE)
+    
+    for(filename in file.list){
+        obs <- read.csv(filename, header=TRUE)
+        good <- complete.cases(obs)
+        if(sum(good) > threshold){
+            complete <- obs[good,]
+            corr <- c(corr, cor(complete[,"sulfate"], complete[,"nitrate"]))
+        }
     }
-  }
-  
-  ## Return a numeric vector of correlations
-  return(corr)
+    
+    ## Return a numeric vector of correlations
+    return(corr)
 }
